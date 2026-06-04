8a1c6c1fcc13721201faa7fa7b85d34b9c3227f3
StimulatingQuantity <mehranalikhatti12@gmail.com>
2026-06-04 11:11:37 +0500

    new update

---
+++ src/HollowRectangle.java
@@ -0,0 +1,18 @@
+public class HollowRectangle {
+    public static void main(String[] args) {
+        int r = 10;
+        int c = 20;
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= c; j++) {
+            if (i == 1 || j == 1 || i == r || j == c)
+            {
+                System.out.print("*");
+            }
+            else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+}
