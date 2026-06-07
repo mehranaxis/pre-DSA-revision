456f6dce02b5440219a4de2bb2faa7227d2e64db
StimulatingQuantity <mehranalikhatti12@gmail.com>
2026-06-07 13:36:12 +0500

    new file

---
+++ src/StringBuilderx.java
@@ -0,0 +1,7 @@
+public class StringBuilderx {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Guddu bhai");
+        System.out.println(str.charAt(2));
+        System.out.println("hi");
+    }
+}
