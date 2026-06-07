21d5d23d49ff39fabf417ad219e939bfde70aaae
StimulatingQuantity <mehranalikhatti12@gmail.com>
2026-06-07 22:50:16 +0500

    new file

--- src/StringBuilderx.java
+++ src/StringBuilderx.java
@@ -2,6 +2,15 @@ public class StringBuilderx {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Guddu bhai");
         System.out.println(str.charAt(2));
-        System.out.println("hi");
+        System.out.println(str);
+
+
+        str.setCharAt(0, 'P');
+        System.out.println(str);
+
+        str.append('n');
+        System.out.println(str);
+
+        
     }
 }
