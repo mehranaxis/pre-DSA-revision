93c70c0698b0572ba7e5cc8d0c5ea1b1c2bea861
StimulatingQuantity <mehranalikhatti12@gmail.com>
2026-06-09 00:46:25 +0500

    new file

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+    public static void print(int[] arr){
+        for (int i = 0; i< arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 3, 1, 2};
+        print(arr);
+        for ( int i = 0; i < arr.length- 1; i++){
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        print(arr);
+    }
+}
